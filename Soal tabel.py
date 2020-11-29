a93d84096b3c566171fd49ce10e6ce67c36c74a9
utsaimin <utsaimin22@gmail.com>
2020-11-30 00:21:32 +0700

    Create Soal tabel.py

---
+++ Soal tabel.py
@@ -0,0 +1,37 @@
+x=10
+y=1.5
+print (type(x+x))
+print (type(x+y))
+print (type(y+x))
+print (type(y+y))
+
+print (type(x-x))
+print (type(x-y))
+print (type(y-x))
+print (type(y-y))
+
+print (type(x*x))
+print (type(x*y))
+print (type(y*x))
+print (type(y*y))
+
+
+print (type(x/x))
+print (type(x/y))
+print (type(y/x))
+print (type(y/y))
+
+print (type(x//x))
+print (type(x//y))
+print (type(y//x))
+print (type(y//y))
+
+print (type(x%x))
+print (type(x%y))
+print (type(y%x))
+print (type(y%y))
+
+print (type(x**x))
+print (type(x**y))
+print (type(y**x))
+print (type(y**y))
