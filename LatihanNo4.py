6284481ed153df261a1191cabf06f69a31bc2f4a
utsaimin <utsaimin22@gmail.com>
2020-11-30 00:21:21 +0700

    Create LatihanNo4.py

---
+++ LatihanNo4.py
@@ -0,0 +1,9 @@
+import math
+jarakPertama=125
+kecepatanPertama=62
+jarakKedua=256
+kecepatanKedua=70
+waktuBerangkat=06.00
+waktuIstirahat=0.45
+pukulSampai =(waktuBerangkat)+(waktuIstirahat)+(jarakPertama)//(kecepatanPertama)+(jarakKedua)//(kecepatanPertama)
+print (pukulSampai)
